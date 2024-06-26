97cf70255e604a05a25a1e3939ea60ba57c421d6
Pedro  Paulo Lima Dias <pedroppld@gmail.com>
2024-06-26 13:55:54 -0300

    adc players e auth

---
+++ auth/[...nextauth]/route.js
@@ -0,0 +1,57 @@
+import { connectMongoDB } from "@/lib/mongodb";
+import User from "@/models/user";
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {},
+
+      async authorize(credentials) {
+        const { email, password } = credentials;
+        try {
+          await connectMongoDB();
+          const user = await User.findOne({ email });
+
+          if (!user) {
+            return null;
+          }
+
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+          if (!passwordsMatch) {
+            return null;
+          }
+
+          return user;     //    { user, role: user.role ?? "user" };
+        } catch (error) {
+          console.log("error", error);
+        }
+      },
+    }),
+  ], 
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.role = user.role;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.role = token.role;
+      return session;
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/",
+  },
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
