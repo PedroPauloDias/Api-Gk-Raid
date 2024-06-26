8dab8b2e7ca23e4627c1b239626f25e4c243be8e
Pedro  Paulo Lima Dias <pedroppld@gmail.com>
2024-06-26 15:12:15 -0300

    vercel.json

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 const mongoDbUri = process.env.MONGODB_URI;
 console.log('MongoDB URI:', mongoDbUri);
 
